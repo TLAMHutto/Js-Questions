e6da4c363c150db8d8719d91d7c899111189898b
TLAMHutto <hutto.keaton@yahoo.com>
2022-05-03 13:03:54 -0500

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+$.ajax({
+    url: "./questions.json",
+    method: "GET",
+    success: function(data){
+        var randomObject = data[Math.floor(Math.random() * data.length)];
+        var question = randomObject.question;
+        var answer = randomObject.answer;
+        $(".question").append(question);
+        $(".answer").append(answer);
+    }
+});
+$("#btnFlip").on('click' ,function(){
+    $(".flip-card .flip-card-inner").toggleClass("flipped");
+    
+});
+//when #btnNext is clicked, the next question is displayed
+$("#btnNext").on('click', function(e){
+    e.preventDefault();
+    $.ajax({
+        url: "./questions.json",
+        method: "GET",
+        success: function(data){
+            var randomObject = data[Math.floor(Math.random() * data.length)];
+            var question = randomObject.question;
+            var answer = randomObject.answer;
+            $(".question").empty().append(question);
+            $(".answer").empty().append(answer);
+        }
+    });
+}
+);
+
+
+
